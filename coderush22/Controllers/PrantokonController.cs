f878d81f7f25105a96c582eef319d54f101355e6
Grant Watson <grant@triumphdesigns.org>
2019-02-24 20:03:27 -0500

    Initial Commit

---
+++ coderush22/Controllers/PrantokonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coderush22.Controllers
+{
+    public class PrantokonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult HomeSlider()
+        {
+            return View();
+        }
+
+        public IActionResult HomeVideo()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogDetail()
+        {
+            return View();
+        }
+    }
+}
